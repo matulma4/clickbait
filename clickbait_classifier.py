f54c76a367e9653cd433297e708b899a20bafe43
matulma4 <matulma4@fel.cvut.cz>
2018-08-24 13:59:35 +0200

    Flask app added

--- clickbait_classifier.py
+++ clickbait_classifier.py
@@ -1,8 +1,16 @@
+import json
+
 import numpy
+import sys
+
+from flask import Response
+from flask import request
 from sklearn import preprocessing, svm
 from sklearn.pipeline import Pipeline
 
 import utility
+from flask import Flask
+app = Flask(__name__)
 
 ###############  Classifier  ############################
 print "creating classifier..."
@@ -32,18 +40,47 @@ def is_click_bait(document):
         # t = scaler.transform(title_vector)
         prediction = classifier.predict(title_vector)
         if prediction[0] == 1:
-            print "Clickbait"
+            return "Clickbait"
         else:
-            print "Normal"
+            return "Normal"
     except Exception, e:
         print "except:", e
 
 
+
+##### Flask app
+
+@app.route('/q', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        result = {}
+        j = request.get_json()
+        try:
+            content = j["input"]
+        except KeyError:
+            return Response("Sentence missing.\nPOST data as JSON dictionary with a key \'input\' .",
+                            status=400)
+        if isinstance(content, (list, tuple)):
+            # {response: [{clickbait:True, sent:blabla}, ...]}
+            output = [{"clickbait":is_click_bait(doc), "sent": doc} for doc in content]
+        else:
+            output = {"clickbait":is_click_bait(content), "sent": content}
+        result = {"response": output}
+        return Response(json.dumps(result), status=200, mimetype='application/json')
+
+
 if __name__ == '__main__':
-    input_text = "W"
-    while input_text != "Q":
-        input_text = raw_input("Enter a news headline or 'Q/q' to exit: ")
-        if input_text == "Q" or input_text == 'q':
-            break
-        is_click_bait(input_text)
-    print "Exited Succesfully"
+    mode = ""
+    if len(sys.argv) > 1:
+        mode = sys.argv[1]
+
+    if mode == "server":
+        app.run(host="0.0.0.0", port=4586)
+    elif mode == "shell":
+        input_text = "W"
+        while input_text != "Q":
+            input_text = raw_input("Enter a news headline or 'Q/q' to exit: ")
+            if input_text == "Q" or input_text == 'q':
+                break
+            print is_click_bait(input_text)
+        print "Exited Succesfully"
