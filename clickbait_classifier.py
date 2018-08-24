67f4ae838516b1d217f70f111f39b658ecb5329e
matulma4 <matulma4@fel.cvut.cz>
2018-08-24 13:07:35 +0200

    Formatting

--- clickbait_classifier.py
+++ clickbait_classifier.py
@@ -1,48 +1,49 @@
-from sklearn import preprocessing,svm
-from sklearn.pipeline import Pipeline
-import nltk
 import numpy
-import subprocess
-import time
+from sklearn import preprocessing, svm
+from sklearn.pipeline import Pipeline
+
 import utility
 
 ###############  Classifier  ############################
 print "creating classifier..."
 no_samples = 10000
-positive = numpy.loadtxt("vectors/positive.csv",  delimiter=',')
-negetive = numpy.loadtxt("vectors/negative.csv", delimiter=',')
-X = numpy.concatenate((positive, negetive), axis=0)
+positive = numpy.loadtxt("vectors/positive.csv", delimiter=',')
+negative = numpy.loadtxt("vectors/negative.csv", delimiter=',')
+X = numpy.concatenate((positive, negative), axis=0)
 p = numpy.ones((no_samples, 1))
 n = numpy.full((no_samples, 1), -1, dtype=numpy.int64)
-Y = numpy.concatenate((p,n), axis=0)
+Y = numpy.concatenate((p, n), axis=0)
 y = Y.ravel()
-#scale
+# scale
 scaler = preprocessing.StandardScaler().fit(X)
-#classifying
+# classifying
 svm_module = svm.SVC()
-classifier = Pipeline(steps= [('svm', svm_module)]) #[('scale', scaler), ('svm', svm_module)])
+classifier = Pipeline(steps=[('svm', svm_module)])  # [('scale', scaler), ('svm', svm_module)])
 classifier.fit(X, y)
-print "Classifer created"
+print "Classifier created"
+
+
 ##############################################################
 
 ##################### Clickbait Classifier Service ###########################
-def isclickbait(document):
-	try:
-		title_vector = numpy.array(utility.create_vector(document)).reshape(1,-1)
-		# t = scaler.transform(title_vector)
-		prediction = classifier.predict(title_vector)
-		if prediction[0] == 1:
-			print "The headline is a Clickbait!"
-		else:
-			print "The headline is not a Clickbait!"
-	except Exception,e:
-		print "except:", e
+def is_click_bait(document):
+    try:
+        title_vector = numpy.array(utility.create_vector(document)).reshape(1, -1)
+        # t = scaler.transform(title_vector)
+        prediction = classifier.predict(title_vector)
+        if prediction[0] == 1:
+            print "Clickbait"
+        else:
+            print "Normal"
+    except Exception, e:
+        print "except:", e
+
 
 if __name__ == '__main__':
-	input_text = "W"
-	while input_text != "Q":
-		input_text = raw_input("Enter a news headline or 'Q/q' to exit: ")
-		if input_text == "Q" or input_text == 'q':
-			break
-		isclickbait(input_text)
-	print "Exited Succesfully"
+    input_text = "W"
+    while input_text != "Q":
+        input_text = raw_input("Enter a news headline or 'Q/q' to exit: ")
+        if input_text == "Q" or input_text == 'q':
+            break
+        is_click_bait(input_text)
+    print "Exited Succesfully"
--- stanford_server.py
+++ stanford_server.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from stanford_corenlp_pywrapper import CoreNLP
 import SocketServer
+
+from stanford_corenlp_pywrapper import CoreNLP
+
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
@@ -9,13 +11,14 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
         print self.data
         if not isinstance(self.data, unicode):
             document = unicode(self.data, 'utf-8')
-        jdoc = ss.parse_doc(document, raw = True)
+        jdoc = ss.parse_doc(document, raw=True)
         self.request.sendall(jdoc)
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9998
     # Enter FULL path to folder containing extracted Stanford Core NLP
-    ss = CoreNLP(configdict={'annotators':'tokenize, ssplit, pos, parse'}, corenlp_jars=["path_to_stanford_corenlp/*"])
+    ss = CoreNLP(configdict={'annotators': 'tokenize, ssplit, pos, parse'}, corenlp_jars=["path_to_stanford_corenlp/*"])
     print "model loaded"
     server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
     server.serve_forever()
